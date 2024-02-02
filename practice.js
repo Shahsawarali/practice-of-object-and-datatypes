20cd2b7316b1e95a38cc477f8cc8a0d2714131e8
IlOvecompUter1$t <shahva2022@gmail.com>
2024-02-03 00:51:28 +0500

    practice

---
+++ practice.js
@@ -0,0 +1,19 @@
+// to create a product of amozan 
+const product ={
+    title: "black pen",
+    rating: 4,
+    isoffer: true,
+    price: 300,
+};
+console.log(product);
+
+// to create profile of any socail media platform
+const profile ={
+    username: "@shahsawarali",
+    isfollow: false,
+    follower: 1233,
+    follwing: 4
+};
+
+console.log(profile);
+console.log(typeof(profile.isfollow));
